17cc252d4cfdf2e54871ee2c58a2a7eb2e51ff3f
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-30 16:39:12 +0200

    TouchableOpacity

--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default class Start extends React.Component {
  constructor(props) {
@@ -16,6 +17,41 @@ return (
         value={this.state.text}
         placeholder='Enter Name'
       />          
+        <Text>Choose a color for your Chat: </Text>
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.box1}
+            onPress={() => {this.setState({color: '#cfcd90'})}}
+          >
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.box2}
+            onPress={() => {this.setState({color: '#cfab90'})}}
+          >
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.box3}
+            onPress={() => {this.setState({color: '#a59d97'})}}
+          >
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.box4}
+            onPress={() => {this.setState({color: '#95a6c1'})}}
+          >
+          </TouchableOpacity>
+        </View>
+        <View style={{ 
+          backgroundColor: this.state.color, 
+          borderStyle: 'solid', 
+          borderWidth: 1, 
+          borderColor: 'gray', 
+          margin: 10, 
+          marginBottom: 15, 
+          width: 50, 
+          height: 50, 
+          borderRadius: 25 }}
+        >
+        </View>
       <Button
         title="Go to chat room"
         onPress={() => this.props.navigation.navigate('Chat')}

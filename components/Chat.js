0ef73d672cf567c8e1c1f75fd83ef452017b72b5
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-30 16:42:17 +0200

    const styles = StyleSheet.create({

--- components/Chat.js
+++ components/Chat.js
@@ -20,5 +20,25 @@ export default class Chat extends React.Component {
       />      
       </View>
     );
-  };
-}
+  }
+}    
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000a0"
+  }
+});
+

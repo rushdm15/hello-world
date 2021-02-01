9c526dbe99180f6bfc1af7c1b1342ce026ae63e7
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-01 21:29:25 +0200

    imported bubble

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import { View, Text, Button, Platform, KeyboardAvoidingView } from 'react-native';
 
 // Chat component also exported

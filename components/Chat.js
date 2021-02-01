0b3fee4bf65c9135e87e13cef72f2beb73311595
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-01 15:21:39 +0200

    chat.js updates

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Platform, KeyboardAvoidingView } from 'react-native';
 
 // Chat component also exported
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+  
 // Shows username in navigator, along with color chosen
   render() {
     let name = this.props.route.params.name; // OR ...
@@ -17,9 +43,41 @@ export default class Chat extends React.Component {
       <Button
         title="Go to Start"
         onPress={() => this.props.navigation.navigate('Start')}
-      />      
+      />     
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+      <GiftedChat
+        renderBubble={this.renderBubble.bind(this)}
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      /> 
       </View>
     );
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+         },
+      ],
+    })
   }
 }    
 

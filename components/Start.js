11bef7d21c926dc146f4f19f6445dd33fe32a5b7
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-01 21:29:05 +0200

    deleted old notes

--- components/Start.js
+++ components/Start.js
@@ -122,22 +122,3 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
 });
-
-// // styling for image background
-//   container: {
-//     flex: 1,
-//     flexDirection: "column"
-//   },
-//   image: {
-//     flex: 1,
-//     resizeMode: "cover",
-//     justifyContent: "center"
-//   },
-//   text: {
-//     color: "white",
-//     fontSize: 42,
-//     fontWeight: "bold",
-//     textAlign: "center",
-//     backgroundColor: "#000000a0"
-//   }
-// });
